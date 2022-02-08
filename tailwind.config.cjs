65b4d6e46b9675aeab136f068ac8937a44f29102
devang47 <72780418+Devang47@users.noreply.github.com>
2022-02-08 08:44:14 +0530

    minor changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,8 +5,8 @@ module.exports = {
 	theme: {
 		colors: {
 			...require('tailwindcss/colors'),
-			dark_primary: '#0E0E0E',
-			dark_secondary: '#161616',
+			dark_primary: '#040407',
+			dark_secondary: '#151515',
 			bright_primary: '#E7E7E9',
 			bright_secondary: '#D0D0D0',
 			purple_tint: '#6A52FF',
