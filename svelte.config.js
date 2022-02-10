31c4583a293e3616623673e973e4010704cc7254
Devang Saklani <72780418+Devang47@users.noreply.github.com>
2022-02-10 19:53:06 +0530

    feat: meta tags added

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import preprocess from 'svelte-preprocess';
-import vercel from '@sveltejs/adapter-vercel';
-// import adapter from '@sveltejs/adapter-static';
+// import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -14,7 +14,13 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: vercel(),
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false
+		}),
 		vite: {
 			resolve: {
 				alias: {
