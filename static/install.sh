dd96bbceebf412eb73754454987bdab677eca0cb
Devang Saklani <72780418+Devang47@users.noreply.github.com>
2022-08-06 14:17:00 +0530

    updated

--- static/install.sh
+++ static/install.sh
@@ -7,7 +7,7 @@ NC='\033[0m' # No RED
 echo -e "${RED} Starting ${ORANGE}install.sh! ${NC} \n"
 
 echo -e "${RED} Updating... ${NC} \n"
-sleep 1
+sleep 2
 sudo apt-get update
 sudo apt-get upgrade
 
@@ -16,42 +16,29 @@ figlet -c "saklani.dev"
 
 
 echo -e "\n ${RED} Installing ${ORANGE} NVM... ${NC} \n"
-sleep 1
+sleep 2
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 echo -e "\n ${RED} Installing ${ORANGE} Node... ${NC} \n"
-sleep 1
+sleep 2
 nvm install node
 
-echo -e "\n ${RED} Installing ${ORANGE} Yarn... ${NC} \n"
-sleep 1
-npm install -g yarn
-
 echo -e "\n ${RED} Installing ${ORANGE} Git... ${NC} \n"
-sleep 1
+sleep 2
 sudo apt-get install git -y
 git --version
 
-echo -e "\n ${RED} Installing ${ORANGE} Docker... ${NC} \n"
-sleep 1
-sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get install docker-ce -y
-sudo docker run hello-world
-docker --version
-
-echo -e "\n ${RED} Installing ${ORANGE} nginx... ${NC} \n"
-sleep 1
+echo -e "\n ${RED} Installing ${ORANGE} Nginx... ${NC} \n"
+sleep 2
 sudo apt-get install nginx -y
 
 echo -e "\n ${RED} Installing ${ORANGE} Go... ${NC} \n"
-sleep 1
+sleep 2
 sudo apt install golang-go -y
 
-echo -e "\n ${ORANGE} Done!! ${NC} \n"
-sleep 1
+echo -e "\n ${ORANGE} Done!! ${NC}"
+echo -e "${RED} Packages Installed: ${ORANGE}Node, NVM, Yarn, Git, Docker, Nginx, Go ${NC} \n"
+sleep 2
