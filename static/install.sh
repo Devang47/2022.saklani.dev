b908fddd6262c5ac81bc87d0fc0ac08cedccb25d
Devang Saklani <72780418+Devang47@users.noreply.github.com>
2022-08-06 13:06:28 +0530

    updated

--- static/install.sh
+++ static/install.sh
@@ -2,26 +2,32 @@ COLOR='\033[1;33m'
 echo -e "${COLOR} Starting install.sh!"
 
 echo -e "${COLOR} Updating..."
+sleep 1
 sudo apt update
 sudo apt upgrade
 
 echo -e "${COLOR} Installing NVM..."
+sleep 1
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 echo -e "${COLOR} Installing Node..."
+sleep 1
 nvm install node
 
 echo -e "${COLOR} Installing Yarn..."
+sleep 1
 npm install -g yarn
 
 echo -e "${COLOR} Installing Git..."
+sleep 1
 sudo apt install git-all
 git --version
 
 echo -e "${COLOR} Installing Docker..."
+sleep 1
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
@@ -31,9 +37,12 @@ sudo docker run hello-world
 docker --version
 
 echo -e "${COLOR} Installing nginx..."
+sleep 1
 sudo apt-get install nginx
 
 echo -e "${COLOR} Installing Go..."
+sleep 1
 sudo apt install golang-go 
 
 echo -e "${COLOR} Done!!"
+sleep 1
