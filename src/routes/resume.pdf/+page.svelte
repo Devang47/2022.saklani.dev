1e17d05efa62cf5665383c7c52d00a6bad1d21ba
Devang Saklani <72780418+Devang47@users.noreply.github.com>
2023-02-04 14:11:35 +0530

    resume bug fixed

---
+++ src/routes/resume.pdf/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		goto('https://docs.google.com/document/d/1tERbObbptuw8QVDyPfJ5FlSshtoeZVSgEBoSYUhlOC4/');
+	});
+</script>
